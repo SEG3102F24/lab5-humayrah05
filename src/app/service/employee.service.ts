4f95e41992fe426d669ae6cfd768e24f2c7e1a68
humayrah <124104680+humayrah05@users.noreply.github.com>
2024-10-20 19:00:07 -0400

    Merged local files into repository

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject, Observable} from "rxjs";
 import {Employee} from "../model/employee";
+import {addDoc, collection, Firestore, getDocs} from "@angular/fire/firestore";
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,53 @@ import {Employee} from "../model/employee";
 export class EmployeeService {
   employees$: BehaviorSubject<readonly Employee[]> = new BehaviorSubject<readonly Employee[]>([]);
 
+  constructor(private firestore: Firestore) {
+    this.fetchEmployees();
+  }
+
   get $(): Observable<readonly Employee[]> {
     return this.employees$;
   }
 
-  addEmployee(employee: Employee) {
-    this.employees$.next([...this.employees$.getValue(), employee]);
-    return true;
+  async addEmployee(employee: Employee) {
+    try {
+      await addDoc(collection(this.firestore, 'employees'), {
+        name: employee.name,
+        dateOfBirth: employee.dateOfBirth,
+        city: employee.city,
+        salary: employee.salary,
+        gender: employee.gender,
+        email: employee.email
+      });
+
+      await this.fetchEmployees();
+    } catch (error) {
+      console.error('Error adding employee:', error);
+      return false; 
+    }
+
+    return true; 
+  }
+
+  async fetchEmployees() {
+    try {
+      const docs = await getDocs(collection(this.firestore, 'employees'));
+
+      const employeesList: Employee[] = docs.docs.map(doc => {
+        const employee = doc.data();
+        return {
+          name: employee['name'],
+          dateOfBirth: employee['dateOfBirth'].toDate(),
+          city: employee['city'],
+          salary: employee['salary'],
+          gender: employee['gender'],
+          email: employee['email'],
+        } as Employee;
+      });
+
+      this.employees$.next(employeesList); 
+    } catch (error) {
+      console.error('Error fetching employees:', error);
+    }
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyA_ucl0PBFQHFz3o5NuKuM5Fx-oGrd1KDI",
+        authDomain: "employee-book-976d9.firebaseapp.com",
+        projectId: "employee-book-976d9",
+        storageBucket: "employee-book-976d9.appspot.com",
+        messagingSenderId: "733189808664",
+        appId: "1:733189808664:web:52586b155f85fc0a7e196d",
+        measurementId: "G-CJF0643GPX"
+    }
+}
